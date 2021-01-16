e4e1bd1a2401ccdaf29aa02d4782330e866cad89
Bayusatryia <77192926+Bayusatryia@users.noreply.github.com>
2021-01-17 06:52:43 +0700

    Add files via upload

---
+++ Src/funmenu.js
@@ -0,0 +1,21 @@
+const funmenu = (prefix) => { 
+	return `
+✎═─⊱〘 𝐹𝑈𝑁 𝑀𝐸𝑁𝑈 〙⊰══
+║
+╰─⊱ *${prefix}tebakgambar* [ Error ]
+╰─⊱ *${prefix}caklontong* [ Error ]
+╰─⊱ *${prefix}family100* [ Error ]
+╰─⊱ *${prefix}game* [ Error ]
+╰─⊱ *${prefix}truth*
+╰─⊱ *${prefix}dare*
+╰─⊱ *${prefix}quotes*
+╰─⊱ *${prefix}hilih*
+╰─⊱ *${prefix}alay* [text]
+╰─⊱ *${prefix}simi* [text]
+╰─⊱ *${prefix}bucin*
+╰─⊱ *${prefix}gtts* [text]
+╰─⊱ *${prefix}tts*
+║
+✎═─⊱〘 Zky BOT 〙⊰══`
+}
+exports.funmenu = funmenu
